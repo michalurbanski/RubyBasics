5123de0274df83f0fb68a7b8213b36057ec000ee
michalurbanski <michal_urbanski@windowslive.com>
2015-08-29 15:02:28 +0200

    Initial commit with strings example

---
+++ strings.rb
@@ -0,0 +1,11 @@
+# first ruby program
+
+puts 'hello world' # Puts ends line with return character
+puts 'This is second line of code'
+
+#print 'Enter your name: ' # Print does not end line with return character
+#name = gets() 
+#puts "Hello #{name}" # string interpolation works only with "" not with '' 
+
+puts 'First string exercise ------- '
+puts ("\n\t#{(1 + 2) * 3}\nGoodbye") # \n is new line character
