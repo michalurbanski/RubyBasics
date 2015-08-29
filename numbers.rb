705da3ca1f3b6527a2c1423a1b88ba6c89815ba1
michalurbanski <michal_urbanski@windowslive.com>
2015-08-29 21:20:17 +0200

    Extends numbers example for if-then clause and methods usage

--- numbers.rb
+++ numbers.rb
@@ -1,10 +1,34 @@
-# numbers example
+=begin
+this is multiline comment
+Begin and end must start and end at the line beginning
+=end
+
+# method not in the class has to be declared before call
+def prompt_user_for_input
+  puts 'Please enter value'
+end
 
 puts 'This is numbers example'
 
 first = 1
 second = 2
+prompt_user_for_input
 input = gets
 total = first + second + input.to_f # to_f converts to floating number
 									# if conversion is not possible than 0.0 is returned during conversion
 puts "first is #{first}, second is #{second}, total is #{total}"
+
+puts '------ if-then example ------'
+prompt_user_for_input
+input = gets
+input = input.to_f
+
+if input > 0
+  puts 'This number is positive'
+else
+  puts 'This number is negative'
+end
+
+if input > 0 then puts 'Once again positive' end # if-then example
+
+
