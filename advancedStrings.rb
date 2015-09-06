c4278957272f4a2e1622fa9b9ae473d09df6ad82
michalurbanski <michal_urbanski@windowslive.com>
2015-09-06 18:46:41 +0200

    Adds object_id examples

--- advancedStrings.rb
+++ advancedStrings.rb
@@ -1,5 +1,10 @@
 # advanced strings operations
 
+def show_delimiter
+  puts '-' * 20
+end
+
+
 # backquotes means that operating system should be invoked
 #puts `ls`
 
@@ -11,10 +16,26 @@ s = "Hello " + "world"
 puts(s)
 s = "Hello " "world"
 puts(s)
-puts '-'*20
+show_delimiter
 
 # comma separator makes an array of string s
 s = "This ", "is ", "strings array"
 print("Print: ", s, "\n")
 puts("The same array displayed using puts: ", s)
 puts("Class of s is: #{s.class}")
+
+# allows to print object_id
+s = "Quite a new string"
+puts "-- Object ids"
+firstObjectId = s.object_id
+puts("Object id is: #{firstObjectId}")
+
+s = s.capitalize # without exclamation mark this assignment will create new line
+secondObjectId = s.object_id
+puts("Object id is: #{secondObjectId}")
+puts("Are object ids equal?: #{firstObjectId == secondObjectId}")
+show_delimiter
+
+
+
+
