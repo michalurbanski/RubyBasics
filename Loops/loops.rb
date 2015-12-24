7586a342b484502bef2be04fe3ab4f9b9ae8c042
michal_urbanski <michal_urbanski@windowslive.com>
2015-12-24 21:51:50 +0100

    For loop implementation

---
+++ Loops/loops.rb
@@ -0,0 +1,19 @@
+# Loops examples
+
+array = [1,2,3]
+
+for i in array do
+  puts(i)
+end
+
+# This can also be written in a concised way
+puts('Concise example')
+for i in array do puts(i) end
+
+# Another way to iterate over array elements
+puts('Another way to iterate')
+array.each do |i|
+  puts(i)
+end
+
+# Standard while loop
